a485191fdc9ccd77563e8e9289d7d9288384c8f8
josejassojr <jose.d.jassojr@gmail.com>
2021-12-07 13:38:31 -0800

    Styling for mobile view.

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -17,13 +17,14 @@ function AboutMe() {
         tellus. In fermentum volutpat elit, eget fermentum metus lacinia vel.
         Sed suscipit tempus nisi, eget eleifend erat elementum quis. Quisque
         sodales egestas egestas. In eleifend nunc id erat varius, sed egestas
-        lacus feugiat. Donec neque tortor, varius eu consequat a, mollis eu
-        nisi. Donec eros elit, tincidunt ac vulputate non, sagittis at purus.
-        Duis in massa sed dolor semper faucibus in eu ex. Quisque aliquet
-        rhoncus nulla quis tincidunt. Sed porta auctor porta. Sed vestibulum
-        urna a ullamcorper pellentesque. Donec et est at justo luctus auctor eu
-        ut orci. Nullam eu eros bibendum, tincidunt diam euismod, aliquet sem.
-        Nunc purus justo, congue eu lacus nec, lacinia bibendum est.
+        lacus feugiat.
+        <br />
+        <br /> Donec neque tortor, varius eu consequat a, mollis eu nisi. Donec
+        eros elit, tincidunt ac vulputate non, sagittis at purus. Duis in massa
+        sed dolor semper faucibus in eu ex. <br />
+        <br />
+        Quisque aliquet rhoncus nulla quis tincidunt. Sed porta auctor porta.
+        Sed vestibulum urna a ullamcorper pellentesque.
       </p>
       <div id="quote-container" className="container">
         <p id="quote">
